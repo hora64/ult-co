d2ca5dc0eecd0231904e671c43af6f98610237b1
hora64 <161913562+hora64@users.noreply.github.com>
2024-03-02 21:52:13 -0500

    Update click.js

--- content/scripts/click.js
+++ content/scripts/click.js
@@ -21,38 +21,45 @@ function lerp(a, b, f) {
   return (b - a) * f + a;
 }
 
-function getRotation() {
-  // Generate a smaller range of angles for a subtler rotation effect.
-  // For example, using angles between -45 and 45 degrees.
-  return `rotate(${rand(-15, 15)}deg)`;
-}
 
+
+var rotationMin = 5; // Minimum rotation in degrees
+var rotationMax = 15; // Maximum rotation in degrees
 
 function heart(x, y) {
-  var s = getSize();
-  x -= s / 2;
-  y -= s / 2;
-  x = Math.floor(x) + rand(-5, 5);
-  y = Math.floor(y) + rand(-5, 5);
-  var fx = x + rand(-40, 40);
-  var fy = y + rand(-40, 40);
-  var i = document.createElement("img");
-  i.src = imgurl[rand(0, imgurl.length - 1)];
-  i.style.cssText = `pointer-events: none; position: fixed; width: ${s}px; left: ${x}px; top: ${y}px; transform: ${getRotation()}; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; z-index: 1000000;`;
-  b.appendChild(i);
-  var f = 0;
-  var interval;
-  interval = setInterval(function () {
-    var _x = Math.floor(lerp(x, fx, f));
-    var _y = Math.floor(lerp(y, fy, f));
-    i.style.cssText = `pointer-events: none; position: fixed; width: ${s}px; left: ${_x}px; top: ${_y}px; transform: ${getRotation()}; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; z-index: 1000000; opacity: ${1 - f};`;
-    f += 0.01;
-    if (f > 1) {
-      clearInterval(interval);
-      b.removeChild(i);
-    }
-  }, 10);
+    var s = getSize();
+    x -= s / 2;
+    y -= s / 2;
+    x = Math.floor(x) + rand(-5, 5);
+    y = Math.floor(y) + rand(-5, 5);
+    var fx = x + rand(-40, 40);
+    var fy = y + rand(-40, 40);
+    var i = document.createElement("img");
+    i.src = imgurl[rand(0, imgurl.length - 1)];
+    var initialRotation = rand(rotationMin, rotationMax);
+    var finalRotation = initialRotation + rand(rotationMin, rotationMax); // Ensures rotation changes as it fades
+    i.style.cssText = getStyle(x, y, s, initialRotation);
+    b.appendChild(i);
+    var f = 0;
+    var interval;
+    interval = setInterval(function () {
+        var _x = Math.floor(lerp(x, fx, f));
+        var _y = Math.floor(lerp(y, fy, f));
+        var currentRotation = lerp(initialRotation, finalRotation, f);
+        i.style.cssText = getStyle(_x, _y, s, currentRotation);
+        i.style.opacity = 1 - f;
+        f += 0.01;
+        if (f > 1) {
+            clearInterval(interval);
+            b.removeChild(i);
+        }
+    }, 10);
 }
+
+function getStyle(x, y, size, rotation) {
+    return `pointer-events: none; position: fixed; width: ${size}px; left: ${x}px; top: ${y}px; transform: rotate(${rotation}deg); -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; z-index: 1000000;`;
+}
+
 
 function bro(x, y) {
   for (var i = 5; i < 9; i++) {
