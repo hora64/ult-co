83c81f49130654479d43356a6809d80f6b68f9ba
hora64 <161913562+hora64@users.noreply.github.com>
2024-03-02 22:41:04 -0500

    Update startUp.js

--- content/scripts/startUp.js
+++ content/scripts/startUp.js
@@ -1,4 +1,4 @@
-document.body.addEventListener('click', function playStartupSound() {
+function playStartupSound() {
     var music = document.getElementById('startup');
     if (music) {
         music.volume = 0.2;
@@ -7,11 +7,15 @@ document.body.addEventListener('click', function playStartupSound() {
         if (promise !== undefined) {
             promise.then(_ => {
                 console.log('start up sound');
-                // Remove the event listener to prevent the sound from playing on every click
-                document.body.removeEventListener('click', playStartupSound);
+                // Successfully started playback
             }).catch(error => {
                 console.error('Failed to play start up sound:', error);
             });
         }
     }
-}, {once: true}); // Ensures the listener is removed after the first execution
+}
+
+// Ensures the event listener is properly added after the document is fully loaded
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.body.addEventListener('click', playStartupSound, {once: true});
+});
