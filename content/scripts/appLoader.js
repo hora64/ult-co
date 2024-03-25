9d195d07ef4cef20e7195d0879f769f673e0924a
hora64 <161913562+hora64@users.noreply.github.com>
2024-03-25 16:12:14 -0400

    Delete content/scripts/appLoader.js

--- content/scripts/appLoader.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$("#app-settings").load("../windows/settings-window.html", function() {
-                // Settings window loaded
-            });
-            $("#app-webamp").load("../windows/webAmp.html", function() {
-                // WebAmp loaded
-      });
-});
