d2b337816520d8c3b289529821261ecac9be00a9
hora64 <161913562+hora64@users.noreply.github.com>
2024-03-30 00:01:41 -0400

    Update dataFunctions.js

--- content/scripts/dataFunctions.js
+++ content/scripts/dataFunctions.js
@@ -1,1 +1,29 @@
+function saveColorSettings(red, green, blue) {
+    localStorage.setItem('colorSettings', JSON.stringify({ red, green, blue }));
+}
 
+function loadColorSettings() {
+    const savedColorSettings = localStorage.getItem('colorSettings');
+    if (savedColorSettings) {
+        const colors = JSON.parse(savedColorSettings);
+        // Return the colors to be applied by the caller
+        return colors;
+    }
+    return null; // No saved settings
+}
+
+function saveSelectedWallpaper(wallpaperPath) {
+    localStorage.setItem('selectedWallpaper', wallpaperPath);
+}
+
+function getSavedWallpaper() {
+    return localStorage.getItem('selectedWallpaper');
+}
+
+function saveSelectedFavicon(faviconPath) {
+    localStorage.setItem('selectedFavicon', faviconPath);
+}
+
+function getSavedFavicon() {
+    return localStorage.getItem('selectedFavicon');
+}
