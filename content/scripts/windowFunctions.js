0b4c3f6a0ca48f29e2054241f9541c694ac49fdb
hora64 <161913562+hora64@users.noreply.github.com>
2024-03-30 19:57:23 -0400

    Update windowFunctions.js

--- content/scripts/windowFunctions.js
+++ content/scripts/windowFunctions.js
@@ -48,29 +48,37 @@ function disableContainer(containerElementId) {
 
 // Function to switch tabs and display corresponding content, defined globally for accessibility
 function switchTab(tabId, tabPath) {
-    const $tabPanels = $('article[role="tabpanel"]');
-    const $tabs = $('menu[role="tablist"] button');
+	const $tabPanels = $('article[role="tabpanel"]');
+	const $tabs = $('menu[role="tablist"] button');
 
-    // Hide all tab panels and mark them as hidden
-    $tabPanels.hide().attr('hidden', true);
+	// Hide all tab panels and mark them as hidden
+	$tabPanels.hide().attr('hidden', true);
 
-    // Load the content for the selected tab using the provided path
-    $('#' + tabId).load(tabPath, function(response, status, xhr) {
-        if (status == "error") {
-            console.error("Failed to load tab content:", xhr.status, xhr.statusText);
-            // Optionally, display an error message within the tab content area
-            $(this).html("Sorry, there was an error loading the content.");
-        } else {
-            // Show the loaded content
-            $(this).show().removeAttr('hidden');
-        }
-    });
+	// Load the content for the selected tab using the provided path
+	$('#' + tabId).load(tabPath, function(response, status, xhr) {
+		if (status == "error") {
+			console.error("Failed to load tab content:", xhr.status, xhr.statusText);
+			// Optionally, display an error message within the tab content area
+			$(this).html("Sorry, there was an error loading the content.");
+		} else {
+			// Show the loaded content
+			$(this).show().removeAttr('hidden');
+		}
+	});
 
-    // Update aria-selected for all tabs
-    $tabs.attr('aria-selected', 'false');
-    $(`button[aria-controls="${tabId}"]`).attr('aria-selected', 'true');
+	// Update aria-selected for all tabs
+	$tabs.attr('aria-selected', 'false');
+	$(`button[aria-controls="${tabId}"]`).attr('aria-selected', 'true');
+}
+// Global function to load tab content
+function loadTabContent(tabId, url) {
+	$(tabId).load(url); // Load content from the specified URL into the tab
 }
 
+// Global function to unload tab content
+function unloadTabContent(tabSelector) {
+	$(tabSelector).empty(); // Empty the content of the specified tab
+}
 
 // jQuery for draggable and resizable behaviors on "#app-settings"
 $(function() {
