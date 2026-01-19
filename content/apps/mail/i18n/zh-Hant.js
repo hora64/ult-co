0dd74d9cda60a096e2bbc54a3e6b45830d46b525
hora64_ <161913562+hora64@users.noreply.github.com>
2026-01-18 21:51:10 -0500

    Added back translations for mail app in Traditional Chinese

--- content/apps/mail/i18n/zh-Hant.js
+++ content/apps/mail/i18n/zh-Hant.js
@@ -6,5 +6,32 @@ export const zhHant = {
       "weight": "normal",
       "scale": 1.15
     }
-  }
+  },
+      "exit": "退出",
+    "unopened": "未打開",
+    "unopenedMail": "未讀郵件",
+    "searchPlaceholder": "搜索...",
+    "noResults": "沒有結果",
+    "close": "關閉",
+    "back": "",
+    "byAuthor": "{author}著",
+    "sortAndFilter": "排序與篩選",
+    "applyAndClose": "套用並關閉",
+    "sortBy": "排序",
+    "newestFirst": "由新到舊",
+    "oldestFirst": "由舊到新",
+    "authorAZ": "作者 (A-Z)",
+    "readStatus": "讀取狀態",
+    "all": "全部",
+    "read": "已讀",
+    "filterByTag": "按標籤篩選",
+    "errorLoadingMail": "郵件載入失敗。",
+    "linkCopied": "成功複製連結",
+    "pinned": "",
+    "pinnedArticle": "",
+    "footer": {
+        "exit": "退出",
+        "applyFilter": "應用篩選",
+        "resetFilter": "重置篩選"
+    }
 };
