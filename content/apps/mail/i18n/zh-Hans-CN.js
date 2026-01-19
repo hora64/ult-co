9697499247b1fc37496b041979f50ecbe4dce4cb
hora64_ <161913562+hora64@users.noreply.github.com>
2026-01-18 21:52:26 -0500

    Added back Chinese translations for mail interface

--- content/apps/mail/i18n/zh-Hans-CN.js
+++ content/apps/mail/i18n/zh-Hans-CN.js
@@ -6,5 +6,32 @@ export const zhHansCN = {
             "weight": "normal",
             "scale": 1.15
         }
-    }
+    },
+    "exit": "退出",
+        "unopened": "未打开",
+        "unopenedMail": "未读邮件",
+        "searchPlaceholder": "搜索...",
+        "noResults": "没有结果",
+        "close": "关闭",
+        "back": "",
+        "byAuthor": "{author}著",
+        "sortAndFilter": "排序与筛选",
+        "applyAndClose": "使用以及关闭",
+        "sortBy": "排序",
+        "newestFirst": "最新排序",
+        "oldestFirst": "最旧排序",
+        "authorAZ": "作者 (A-Z)",
+        "readStatus": "读取状态",
+        "all": "全部",
+        "read": "已读",
+        "filterByTag": "按标签筛选",
+        "errorLoadingMail": "邮件加载失败。",
+        "linkCopied": "成功复制链接",
+        "pinned": "",
+        "pinnedArticle": "",
+        "footer": {
+            "exit": "退出",
+            "applyFilter": "使用筛选",
+            "resetFilter": "重置筛选"
+        }
 };
